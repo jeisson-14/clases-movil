a36820e4f3fe0b75bfec4e1038c5a4e6c41d2aab
jeisson-14 <thedavid147@gmail.com>
2025-04-25 01:33:41 +0000

    Pending changes exported from your codespace

--- Clases-movil/api/auth.js
+++ Clases-movil/api/auth.js
@@ -5,7 +5,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const login = async (email, password) => {
   try {
-    const response = await apiClient.post('https://5c36d0e5-e9b6-4bb0-888f-e57d6f571338-00-2i7rz9xf7vcjb.picard.replit.dev/api/auth/login', { email, password });
+    const response = await apiClient.post('https://ea850373-2826-4baf-9b8e-cdfea809902a-00-brfit0bk59uu.riker.replit.dev/api/auth/login', { email, password });
     const { token } = response.data;
     await AsyncStorage.setItem('token', token);
     return response.data;
@@ -16,7 +16,7 @@ export const login = async (email, password) => {
 
 export const register = async (userData) => {
   try {
-    const response = await apiClient.post('https://5c36d0e5-e9b6-4bb0-888f-e57d6f571338-00-2i7rz9xf7vcjb.picard.replit.dev/api/auth/register', userData);
+    const response = await apiClient.post('https://ea850373-2826-4baf-9b8e-cdfea809902a-00-brfit0bk59uu.riker.replit.dev/api/auth/register', userData);
     return response.data;
   } catch (error) {
     throw error.response ? error.response.data : error;
@@ -25,7 +25,7 @@ export const register = async (userData) => {
 
 export const logout = async () => {
   const token = await AsyncStorage.getItem('token');
-  return await axios.post(`https://5c36d0e5-e9b6-4bb0-888f-e57d6f571338-00-2i7rz9xf7vcjb.picard.replit.dev/api/auth/logout`, {}, {
+  return await axios.post(`https://ea850373-2826-4baf-9b8e-cdfea809902a-00-brfit0bk59uu.riker.replit.dev/api/auth/logout`, {}, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
